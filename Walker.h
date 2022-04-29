33ff98828859cd28ef97c96b7a4df952b262763c
한주형 <>
2022-04-29 04:39:36 +0000

    Initial commit

---
+++ Walker.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "usr/include/SDL2/SDL.h"
+#include "usr/include/SDL2/SDL2_gfxPrimitives.h"
+
+#include "InputHandler.h"
+#include "Vector2D.h"
+#include "Game.h"
+
+#include "Vehicle.h"
+#include <vector>
+
+class Walker 
+{
+public:
+  Walker();
+  void draw(SDL_Renderer* renderer);
+  void update();
+  void mousemove();
+
+private:
+  std::vector<Vehicle*> m_Vehicle;
+  Vector2D* m_mousePos;
+  Vector2D* target;
+};
